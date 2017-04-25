595aa748d8f0addef6efb8dd20975a70f088bc22
cbdweb <nik@cbdweb.net>
2017-04-26 09:51:18 +1000

    https://

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 
 			<div class="site-info">
 				<?php do_action( 'twentythirteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s with custom software by <a href="http://www.cbdweb.net" target="_blank">CBDWeb</a>. Hire us as your webmasters.', 'twentythirteen' ), 'WordPress' ); ?></a>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s with custom software by <a href="https://www.cbdweb.net" target="_blank">CBDWeb</a>. Hire us as your webmasters.', 'twentythirteen' ), 'WordPress' ); ?></a>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
